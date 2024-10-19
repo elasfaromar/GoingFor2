7ea2a93f185b38671982a4af8466f9f242da46c0
Omar Elasfar <omarelasfaregy17@gmail.com>
2024-10-19 14:13:04 -0400

    chall

--- service/src/services/Timetable.ts
+++ service/src/services/Timetable.ts
@@ -2,6 +2,7 @@ import { Timetable } from "@prisma/client";
 import { prisma } from "../db";
 import { Result, Ok, Err } from "ts-results";
 import { AccountService } from ".";
+const nodemailer = require("nodemailer");
 
 export const createTimetable = async (
   email: string,
@@ -32,6 +33,37 @@ export const createTimetable = async (
         })),
       },
     },
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "",
+      pass: "",
+    },
+  });
+
+  const mailOptions = {
+    from: "",
+    to: email,
+    subject: "Time Table Confirmation",
+    text: `Below is your timetable information:\n\n
+    Name: ${timetable.name}\n
+    Table ID: ${timetable.id}\n
+    Student ID: ${timetable.accountId}\n
+    Created: ${timetable.createdAt}\n
+    Updated: ${timetable.updatedAt}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email: ", error);
+    } else {
+      console.log("Email sent: ", info.response);
+    }
   });
 
   return Ok(timetable);
