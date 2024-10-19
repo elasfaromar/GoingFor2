a3a4decc5b7ab714a3590f36df5c118f51753c5c
Omar Elasfar <omarelasfaregy17@gmail.com>
2024-10-19 12:40:47 -0400

    Challenge 3

--- client/src/layouts/Central/Banner/Banner.tsx
+++ client/src/layouts/Central/Banner/Banner.tsx
@@ -3,9 +3,9 @@ import "./Banner.style.scss";
 function Banner() {
   return (
     <div className="Banner">
-      <img src="/carleton_logo.png" alt="logo" />
+      <img src="/carleton_central_logo_black.png" alt="logo" />
       <div className="Banner__seperator"></div>
-      <div className="Banner__title">Carleton Central</div>
+      <div className="Banner__title">Carleton Central, but Better</div>
     </div>
   );
 }
--- client/src/layouts/Central/HeaderLinks/HeaderLinks.tsx
+++ client/src/layouts/Central/HeaderLinks/HeaderLinks.tsx
@@ -6,11 +6,11 @@ function HeaderLinks() {
   const { logout } = useAccountContext();
   return (
     <div className="HeaderLinks">
-      <Link to={"/"}>Return To Menu</Link>|<Link to={"/sitemap"}>Site Map</Link>
-      |<Link to={"/help"}>Help</Link>|
+      <Link to={"/"}>Return To Menu</Link><img className="mini-logo" src="back_button.png"></img>|<Link to={"/sitemap"}>Site Map</Link><img className="mini-logo" src="map_button.png"></img>
+      |<Link to={"/help"}>Help</Link><img className="mini-logo" src="help_button.png"></img>|
       <Link to={"/login"}>
         <span onClick={() => logout()}>Logout</span>
-      </Link>
+      </Link><img className="mini-logo" src="logout.png"></img>
     </div>
   );
 }
