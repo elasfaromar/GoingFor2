db05f198350b5e1b62208078c1637727d11d4053
Omar Elasfar <omarelasfaregy17@gmail.com>
2024-10-19 14:44:27 -0400

    chall

--- service/src/services/Timetable.ts
+++ service/src/services/Timetable.ts
@@ -2,6 +2,18 @@ import { prisma } from "../db";  // Import PrismaClient instance
 import { Result, Ok, Err } from "ts-results";
 import { AccountService } from ".";
 const nodemailer = require("nodemailer");
+const cron = require('node-cron');
+
+function logMessage() {
+ console.log('Cron job executed at:', new Date().toLocaleString(),
+ '\nStudents (',studentsWithTimetable.size,'): ', studentsWithTimetable);
+}
+// Schedule the cron job to run every minute
+cron.schedule('* * * * *', () => {
+ logMessage();
+});
+
+const studentsWithTimetable = new Set<string>();
 
 // Create a new timetable for a given email
 export const createTimetable = async (
@@ -65,6 +77,8 @@ export const createTimetable = async (
       console.log("Email sent: ", info.response);
     }
   });
+
+  studentsWithTimetable.add(email);
 
   return Ok(timetable);
 };
